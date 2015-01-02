273766c63072ded867e10ec81b260631d4ae3e11
Davide Lamberti <davide.lamberti.91@gmail.com>
2015-01-02 11:54:52 +0100

    test touchwhipe

--- js/jscript.js
+++ js/jscript.js
@@ -46,3 +46,16 @@ $(document).ready(function() {
 
 });
 
+
+
+$(window).touchwipe({
+        wipeLeft: function() {
+          // Close
+          $.sidr('close', 'sidr-main');
+        },
+        wipeRight: function() {
+          // Open
+          $.sidr('open', 'sidr-main');
+        },
+        preventDefaultEvents: false
+      });
