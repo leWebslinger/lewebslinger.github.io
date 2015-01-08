274798fb8f763250c4599f35312cd9f48fe2a887
Davide Lamberti <davide.lamberti.91@gmail.com>
2015-01-08 09:39:59 +0100

    migliorie
    adesso è tutto pronto

--- js/jscript.js
+++ js/jscript.js
@@ -1,63 +1,61 @@
 // JavaScript Document
 
 var closePanel = function () {
-	dir_panel = document.getElementById('directions-panel');
-	dir_panel.style.display = "none";
+dir_panel = document.getElementById('directions-panel');
+dir_panel.style.display = "none";
 
 
-	map_panel = document.getElementById('map-canvas');
-	map_panel.style.height = "100%";
+map_panel = document.getElementById('map-canvas');
+map_panel.style.height = "100%";
 
 };
 
-    $(document).ready(function () {
+$(document).ready(function () {
 
-        var makeAllFormSubmitOnEnter = function () {
-            $('#address').live('keypress', function (e) {
-				
-                if (e.which && e.which == 13) {
-                    codeAddress();
-                    return false;
-                } else {
-                    return true;
-                }
-            });
-        };
+var makeAllFormSubmitOnEnter = function () {
+    $('#address').live('keypress', function (e) {
 
-        makeAllFormSubmitOnEnter();
+        if (e.which && e.which == 13) {
+            codeAddress();
+            return false;
+        } else {
+            return true;
+        }
     });
+};
+
+makeAllFormSubmitOnEnter();
+});
 
 
 
 
 
 $(document).ready(function() {
-  $('#simple-menu').sidr({
-      side: 'right'
-  
-  });
+$('#simple-menu').sidr({
+side: 'right'
+
+});
 });
 
 $(document).ready(function() {
-    $('#responsive-menu-button').sidr({
-      name: 'sidr-main',
-      source: '#navigation'
-    });
-    
-    $(window).touchwipe({
-        wipeLeft: function() {
-          // Close
-          $.sidr('close', 'sidr-main');
-        },
-        wipeRight: function() {
-          // Open
-          $.sidr('open', 'sidr-main');
-        },
-        preventDefaultEvents: false
-      });
-
+$('#responsive-menu-button').sidr({
+name: 'sidr-main',
+source: '#navigation'
 });
 
+$(window).touchwipe({
+wipeLeft: function() {
+  // Close
+  $.sidr('close', 'sidr-main');
+},
+wipeRight: function() {
+  // Open
+  $.sidr('open', 'sidr-main');
+},
+preventDefaultEvents: false
+});
 
+});
 
 
